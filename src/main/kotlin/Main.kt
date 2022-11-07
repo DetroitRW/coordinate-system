698312ddb05f505eb7c357a5c23f463a5b866e75
Ilya Brant <detroitib31@gmail.com>
2022-11-07 18:48:38 +0600

    Написал сканнер

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,7 @@
+import java.util.Scanner
+
 fun main(args: Array<String>) {
-    println("Hello World!")
+    val reader = Scanner(System.`in`)
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
