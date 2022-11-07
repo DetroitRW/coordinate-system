68ce21632d353e19647678fbacde6685dbae8dbc
Ilya Brant <detroitib31@gmail.com>
2022-11-07 18:49:04 +0600

    Удалил ненужный код

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,7 +3,4 @@ import java.util.Scanner
 fun main(args: Array<String>) {
     val reader = Scanner(System.`in`)
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
 }
